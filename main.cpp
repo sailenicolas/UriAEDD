491b0943ad7cf7ebe38a22dc7a22a5b619f8e4ed
Elias Nicolas <Dakotathoughs@gmail.com>
2021-04-13 21:32:17 -0300

    Resolved

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int a, b;
+    cin >> a;
+    cin >> b;
+    cout << "SOMA = " << a+b<<endl;
     return 0;
 }
