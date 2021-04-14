a789dddecfb0efd8c710f9509e7ff11c0a45a165
Elias Nicolas <Dakotathoughs@gmail.com>
2021-04-14 06:09:34 -0300

    Fixed Readme and gitignore

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int a, b;
+    cin >> a;
+    cin >> b;
+    cout<<"X = " << a+b<<endl;
     return 0;
 }
