f931bb50eecdc70e5e8642840d2b3e4b011bbc92
Elias Nicolas <Dakotathoughs@gmail.com>
2021-04-13 21:16:19 -0300

    Start

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+}
