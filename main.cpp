51c270e507e8c83b1675dadd4f2005cda8b9f007
Elias Nicolas <sailenicolas@users.noreply.github.com>
2021-04-17 00:54:35 -0300

    My Answer

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <iomanip>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int a, b, d, f;
+    double c, e;
+    cin >> a>>b>>c;
+
+    cin >> d>>f>>e;
+
+    cout << "VALOR A PAGAR: R$ "<< std::fixed << std::setprecision(2)<< (b*c)+(f*e)<<endl;
     return 0;
 }
