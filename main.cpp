1c15a713d089eb5ebc81c5bdf5cae1ef5fcbf7e5
Elias Nicolas <sailenicolas@users.noreply.github.com>
2021-04-17 01:21:07 -0300

    My Answer

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
-
+#include <cmath>        // std::abs
+int compare(int a, int b);
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int a, b, c,d;
+std::cin >> a>>b>>c;
+d = compare(a,b);
+d = compare(c, d);
+std::cout << d << " eh o maior"<< std::endl;
+
+return 0;
+}
+int compare(int a,int b){
+    int d = (a+b+std::abs((a-b)));
+    return d/2;
 }
