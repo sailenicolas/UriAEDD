637e4dde987f5169ebdb613dead10c3fd269db5f
Elias Nicolas <sailenicolas@users.noreply.github.com>
2021-04-17 01:32:29 -0300

    My Answer

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+     double a, b,c, d, r;
+    cin >> a>>b;
+    cin >> c>>d;
+    r = sqrt(pow(c-a, 2)+pow(d-b,2));
+    cout <<fixed << setprecision(4) << r<<endl;
     return 0;
 }
