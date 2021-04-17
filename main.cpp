b547b93d935fa4f57179b92357dfa5a423282614
Elias Nicolas <sailenicolas@users.noreply.github.com>
2021-04-17 02:04:32 -0300

    My Answer

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int a;
+    cin >> a;
+    cout << a * 2<<" minutos" <<endl;
     return 0;
 }
