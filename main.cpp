c1561d7905c677e6d4a4d59e4399f265bdc289fb
Elias Nicolas <sailenicolas@users.noreply.github.com>
2021-04-22 09:52:46 -0300

    My answer

--- main.cpp
+++ main.cpp
@@ -9,6 +9,19 @@ int main() {
      * Code your solution here
      * Escriba su solución aquí
      */
+    double a,b,c, res1,res2,bottom,sqr1;
+    cin >>a>>b>>c;
+    sqr1=pow(b,2)-4*a*c;
+
+    if(a==0|| sqr1<0){
+        cout << "Impossivel calcular"<<endl;
+    }else{
+        res1= -b+ sqrt(sqr1);
+        res2= -b- sqrt(sqr1);
+        bottom= 2*a;
+        printf("R1 = %.5f\n",res1/bottom);
+        printf("R2 = %.5f\n", res2/bottom);
+    }
 
     return 0;
 }
