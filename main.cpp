162ae613627f14ead1b1883fb919d0d8e7ebc8ed
Elias Nicolas <sailenicolas@users.noreply.github.com>
2021-04-22 07:45:10 -0300

    My answer

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ int main() {
      * Code your solution here
      * Escriba su solución aquí
      */
-
+    double a, pi=3.14159;
+    cin >> a;
+    printf("VOLUME = %.3f\n", pow(a,3)*(4/3.0)*pi);
     return 0;
 }
