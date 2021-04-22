db5a2f47e174ca8ba7b2ea19135f0fbc404126bd
Elias Nicolas <sailenicolas@users.noreply.github.com>
2021-04-22 07:57:34 -0300

    Start

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+using namespace std;
+
+int main() {
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+
+    return 0;
+}
