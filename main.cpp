7bbd0082990a46dfc2b9bfa01f20780019f46eda
Elias Nicolas <sailenicolas@users.noreply.github.com>
2021-04-22 09:44:36 -0300

    Start

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+
+    return 0;
+}
