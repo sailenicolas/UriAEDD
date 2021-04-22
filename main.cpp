aaa010701713ecafc026d871a5f13a2ef66b2bda
Elias Nicolas <sailenicolas@users.noreply.github.com>
2021-04-22 07:44:59 -0300

    Start

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+
+    return 0;
+}
